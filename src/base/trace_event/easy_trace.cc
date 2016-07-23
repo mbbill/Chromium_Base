3c4af506830db0e13f55e379f8377e3a00090160
Bill Ming <mbbill@gmail.com>
2016-07-24 00:52:40 +0800

    Added easy_trace.h/cpp

---
+++ src/base/trace_event/easy_trace.cc
@@ -0,0 +1,68 @@
+// billming
+
+#include "base/trace_event/easy_trace.h"
+
+#include "base/files/file_util.h"
+#include "base/logging.h"
+#include "base/memory/ref_counted_memory.h"
+#include "base/run_loop.h"
+#include "base/trace_event/trace_buffer.h"
+#include "base/trace_event/trace_event.h"
+#include "base/trace_event/trace_log.h"
+using namespace std;
+using namespace base;
+
+namespace base {
+
+namespace trace_event {
+
+// leaking this object is fine.
+static const FilePath& path_ = *(new FilePath(FILE_PATH_LITERAL("trace.json")));
+static const char EVENT_CATEGORIES[] = \
+"JSC,webkit,minibrowser,rendering,dom,css,html,loader,platform,platform_graphics";
+
+static void OnTraceDataCollected(
+    Closure quit_closure,
+    trace_event::TraceResultBuffer* buffer,
+    const scoped_refptr<RefCountedString>& json_events_str,
+    bool has_more_events) {
+    buffer->AddFragment(json_events_str->data());
+    if (!has_more_events) {
+        quit_closure.Run();
+    }
+}
+
+static void TraceOutputCallback(const std::string& data) {
+    AppendToFile(path_, data.c_str(), data.length());
+}
+
+void traceStart() {
+    string categories = EVENT_CATEGORIES;
+
+    trace_event::TraceLog::GetInstance()->SetEnabled(
+        trace_event::TraceConfig(categories, trace_event::RECORD_UNTIL_FULL),
+        trace_event::TraceLog::RECORDING_MODE);
+}
+
+void traceStop() {
+    const string _begin = "{\"traceEvents\": [";
+    const string _end = "]}";
+
+    WriteFile(path_, _begin.c_str(), _begin.length());
+
+    trace_event::TraceLog::GetInstance()->SetDisabled();
+
+    trace_event::TraceResultBuffer buffer;
+    buffer.SetOutputCallback(Bind(&TraceOutputCallback));
+
+    RunLoop run_loop;
+    trace_event::TraceLog::GetInstance()->Flush(
+        Bind(&OnTraceDataCollected, run_loop.QuitClosure(), Unretained(&buffer)));
+
+    run_loop.Run();
+    AppendToFile(path_, _end.c_str(), _end.length());
+}
+
+} // namespace trace_event
+} // namespace base
+
---
+++ src/base/trace_event/easy_trace.h
@@ -0,0 +1,19 @@
+// billming
+
+#ifndef BASE_TRACE_EVENT_EASY_TRACE_H
+#define BASE_TRACE_EVENT_EASY_TRACE_H
+
+#include "base/base_export.h"
+
+namespace base {
+
+namespace trace_event {
+
+BASE_EXPORT void traceStart();
+
+BASE_EXPORT void traceStop();
+
+} // namespace trace_event
+} // namespace base
+
+#endif //BASE_TRACE_EVENT_EASY_TRACE_H
